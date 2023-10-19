6563598fa44ff43c201f830f1e2abc64769a6b87
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-10-19 15:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ function decimalToBinary(num) {
   //Write you code here
   
 }
+function decimalToBinary(num) {
+  let output = "";
+  while (num >= 2) {
+    let remainder = num % 2;
+    output = remainder + output;
+    num = parseInt(num / 2);
+  }
+  if (num === 1) {
+    output = 1 + output;
+  }
+  console.log(output);
+}
+
+// let num = prompt("Enter a decimal number: ");
+// decimalToBinary(num)
+
 
 window.decimalToBinary = decimalToBinary;
